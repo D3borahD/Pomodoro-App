5969222f7802c575be11439660e35d5883cf8124
Deb <deborah.douillet@yahoo.fr>
2021-02-04 17:42:52 +0100

    ajout du timer

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ setInterval(()=> {
     }
 }, 100)
 
+// Timer //
+
+function getInputValue() {
+    let choice = document.getElementById('choice').value;
+    let startingMinutes = choice ;
+    this.choice.Value = ('');
+
+    let time = startingMinutes * 60;
+
+    const countDownE1 = document.getElementById('countdown');
+
+    setInterval(updateCountDown, 1000);
+
+    function updateCountDown() {
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countDownE1.innerHTML = `${minutes} : ${seconds}`;
+        if (time > 0) {
+            time--;
+        }else {
+            countDownE1.innerHTML = `Pause`
+        };
+    };
+
+
+}
