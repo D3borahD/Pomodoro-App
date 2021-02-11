478335188bcb840f1ea237a46d91c44f55dac59f
Deb <deborah.douillet@yahoo.fr>
2021-02-11 18:47:58 +0100

    paramettrage bouton

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const pourcent = document.querySelector('.num');
 let counter = 0;
+const compteur = document.querySelector('input');
 
 setInterval(()=> {
     if(counter == 100) {
@@ -17,47 +18,65 @@ function getInputValue() {
     let choice = document.getElementById('choice').value;
     let startingMinutes = choice ;
     this.choice.Value = ('');
-
     let time = startingMinutes * 60;
-
     const countDownE1 = document.getElementById('countdown');
-
     setInterval(updateCountDown, 1000);
+
+
+
+
 
     function updateCountDown() {
         const minutes = Math.floor(time/60);
         let seconds = time % 60;
-
         seconds = seconds < 10 ? '0' + seconds : seconds;
         countDownE1.innerHTML = `${minutes} : ${seconds}`;
         if (time > 0) {
             time--;
-
         }else {
             countDownE1.innerHTML = `Pause`
         };
     };
+
+// action bouton start stop reset //
+
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+const reset = document.querySelector('.reset');
+
+start.addEventListener('click', () => {
+    if(start.value === "active"){
+        start.value = "off";
+        stop.disabled = true;
+        reset.disabled = true;
+        compteur.disabled = 'disabled';
+
+    } else {
+        start.value = "active";
+        compteur.disabled = false ;
+        stop.disabled = false;
+        reset.disabled = false;
+    }
+
+})
+
+
+
 }
 
-// toggler Start Stop
 
-const btn = document.querySelector('.btn');
-const txt = document.querySelector('p');
 
-btn.addEventListener('click', updateBtn);
 
-function updateBtn() {
-  if (btn.value === 'stop') {
-    btn.value = 'start';
-    btn.textContent = 'start';
-  } else {
-    btn.value = 'stop';
-    btn.textContent = 'stop';
-  
-  };
-}
 
-// Reset:
+
+ // Reset:
+//  const reset = document.querySelector('.reset');
+
+//  function reset() {
+//      if (reset.value === 'reset') {
+//  startingMinutes == 0;
+//      }
+//  }
 
 // const reset = document.querySelector('.reset');
 
